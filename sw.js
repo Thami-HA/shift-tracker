7df2a0a9607758411430ece4c8bce05a4441ce32
Thami-HA <dlaminit@hotmail.com>
2026-03-08 14:36:56 +0200

    Implement service worker event listeners
    Added event listeners for install, activate, and fetch in the service worker.

--- sw.js
+++ sw.js
@@ -1,2 +1,14 @@
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  // A basic pass-through fetch handler is required for PWA installation
+  event.respondWith(fetch(event.request));
+});
+
 
